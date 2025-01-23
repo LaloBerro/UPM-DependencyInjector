0d49f5a75dc19ad27cc64a48fad7178b5a09e0c4
Lalo <laurencioberro@hotmail.com>
2025-01-23 12:04:04 -0300

    [A] Added Create menu Item for Injectors

---
+++ Editor/InjectorCreator/InjectorCreator.cs
@@ -0,0 +1,32 @@
+﻿using DependencyInjector.Installers;
+using UnityEditor;
+using UnityEngine;
+
+namespace DependencyInjectorEditor
+{
+    public class InjectorCreator : Editor
+    {
+        private static void CreateNewGameObject<TComponent>() where TComponent : MonoBehaviour
+        {
+            GameObject newGameObject = new GameObject(typeof(TComponent).Name);
+            TComponent component = newGameObject.AddComponent<TComponent>();
+            
+            if(Selection.activeGameObject != null)
+                component.transform.SetParent(Selection.activeGameObject.transform);
+            
+            Selection.activeGameObject = component.gameObject;
+        }
+        
+        [MenuItem("GameObject/DependencyInjection/MonoInjector", false, 0)]
+        private static void CreateMonoInjector()
+        {
+            CreateNewGameObject<MonoInjector>();
+        }
+        
+        [MenuItem("GameObject/DependencyInjection/InjectorsInitializer", false, 0)]
+        private static void CreateInjectorInitializer()
+        {
+            CreateNewGameObject<InjectorsInitializer>();
+        }
+    }
+}
